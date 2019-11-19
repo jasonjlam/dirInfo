3091e3de305e00ff27cee229a261282d9c9f3f7d
Jason <jason@ubuntu.ubuntu-domain>
2019-11-19 00:14:43 -0500

    Finished assignment (for real this time)

--- dir.c
+++ dir.c
@@ -2,34 +2,43 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <errno.h>
 #include <dirent.h>
 
 void printDirectory(char path[200]) {
   int size = 0;
     DIR *directory = opendir(path);
     struct dirent *currentFile = readdir(directory);
+    // Checking if the directory can be opened or not
+    if (currentFile == NULL) {
+        printf("Error opening directory: errno %d", errno);
+        return;
+    }
+    // Sums up the sizes
     struct stat file;
     // printf("Statistic for directory: %s\n", path);
     for (; currentFile !=NULL; currentFile = readdir(directory), stat(currentFile->d_name, &file)) {
+        // printf("%ld \n", file.st_size);
         size += (file.st_size);
     }
+    // Checks all subdirectories (recursive implementation to be done later)
     rewinddir(directory);
-    currentFile = readdir(directory);
     printf("Directories:\n");
-    for (; currentFile !=NULL; currentFile = readdir(directory)) {
+    for (currentFile = readdir(directory); currentFile !=NULL; currentFile = readdir(directory)) {
         // printf("%d \n", currentFile ->d_type);
         if (currentFile->d_type == DT_DIR) {
             printf("\t %s \n", currentFile->d_name);
         }
     }
+    // Now checks for files
     printf("Regular files: \n");
     rewinddir(directory);
-    currentFile = readdir(directory);
-    for (; currentFile !=NULL; currentFile = readdir(directory)) {
+    for (currentFile = readdir(directory); currentFile !=NULL; currentFile = readdir(directory)) {
         // ("%d \n", currentFile ->d_type);
         if (currentFile->d_type == DT_REG) {
-            printf("\t %s \n", currentFile->d_name);
+            printf("\t %s\n", currentFile->d_name);
         }
     }
+    closedir(directory);
     printf("\nTotal Directory Size: %d Bytes \n", size);
 }
--- main.c
+++ main.c
@@ -3,8 +3,27 @@
 #include <sys/stat.h>
 #include <time.h>
 #include "dir.h"
-
-int main() {
-    printDirectory(".");
+#include <errno.h>
+// int argc, char *argv[]
+int main(int argc, char *argv[]) {
+    // Deprecated test for its own directory; comment out if needed
+    // printDirectory("./");
+    if (argc == 2) {
+        printf("%s\n", argv[1]);
+        printDirectory(argv[1]);
+    }
+    else {
+        char dir[256];
+        printf("Enter a directory:\n");
+        fgets(dir, 256, stdin);
+        int i = 0;
+        for (i; dir[i] != '\0'; i++) {
+            if (dir[i] == '\n') {
+                dir[i] = '\0';
+            }
+        }
+        printf("%s\n",dir);
+        printDirectory(dir);
+    }
     return 0;
 }
